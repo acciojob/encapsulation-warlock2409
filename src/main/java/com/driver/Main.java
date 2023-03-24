7a915e912a14a01ed9bef6bd950e49f7d9c4ba0d
rajaram <rajaram@runloyal.com>
2023-03-24 16:36:06 +0530

    enscapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Rajaraman");
+        String name=obj.getName();
+        System.out.print(name);
+
+
+    }
 }
